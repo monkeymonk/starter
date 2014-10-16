1e022659d6f423c0acaaf63c22ddfd9635d88488
Stéphan Zych <stephan@monkeymonk.be>
2014-10-17 01:21:52 +0200

    update build

--- www/js/app.js
+++ www/js/app.js
@@ -3,4 +3,4 @@
  * https://github.com/monkeymonk/starter
  * Copyright (c) 2014 Stéphan Zych
  */
-!function(a){"use strict";var b=function(){$("<link/>",{rel:"stylesheet",type:"text/css",href:"css/plugins.css"}).appendTo("head"),console.log("# initialized",a.location.href.getUrlVars())};requirejs.config({baseUrl:"js",paths:{jquery:"vendor/jquery.min"}}),requirejs(["jquery","plugins","utils"],function(a){a(document).ready(b)})}(window);
+!function(){"use strict";var a=function(){$("<link/>",{rel:"stylesheet",type:"text/css",href:"css/plugins.css"}).appendTo("head"),console.log("# initialized")};requirejs.config({baseUrl:"js",paths:{jquery:"vendor/jquery.min"}}),requirejs(["jquery","plugins","utils"],function(b){b(document).ready(a)})}(window);
